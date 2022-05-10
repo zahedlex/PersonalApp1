8dfc9f330c85720b5d0be349b466de6ac80cd8ae
Elev <Elev@DESKTOP-4985P8J>
2022-05-10 16:20:59 +0200

    Add project files.

---
+++ PersonalApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Personal
+{
+    public class Per
+    {
+        public string Name { get; set; }
+        public int Sallary { get; set; }
+            public Per(string name, int sallary)
+        {
+            Name = name; 
+            Sallary = sallary;
+        }
+
+        public void GetPer()
+        {
+            Console.WriteLine("Name: {0}, Sallary: {1}", Name, Sallary);
+                }
+    }
+    static void Main(string[] args)
+    {
+        
+        Per per = new Per(Console.ReadLine(GetPer));
+        per.GetPer();
+        Console.WriteLine(Name); 
+        Console.WriteLine(Sallary);
+
+    }
+}
